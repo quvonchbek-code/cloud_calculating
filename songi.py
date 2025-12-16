fdb9d737f974ef87611b35d5906a67d15317420f
Quvonchbek <boboxonovmirkomil900@gmail.com>
2025-12-17 00:53:28 +0500

    tugatildi

--- songi.py
+++ songi.py
@@ -24,3 +24,32 @@ def aniq_romb_yigindi(matrix):
                 yigindi += matrix[i][j]
 
     return yigindi
+
+def ikkala_diagonal_yigindi(matrix: np.ndarray) -> tuple[int, int]:
+    
+    asosiy = np.trace(matrix)
+    qarama_qarshi = np.trace(np.fliplr(matrix))
+    return asosiy + qarama_qarshi - matrix[4][4]
+
+def ikkita_uchburchak_yigindi(matrix):
+    n = matrix.shape[0]
+    mid = n // 2
+    yigindi = 0
+
+    for i in range(n):
+        if i <= mid:
+            start = i
+            end = n - i
+        else:
+            start = n - i - 1
+            end = i + 1
+
+        for j in range(start, end):
+            yigindi += matrix[i][j]
+
+    return yigindi
+    
+print("Eng chetki hadlarining yig`indisi : ",chetki_yigindi(matrix))
+print("Diagonallarda joylashgan elementlar yig`indis : ",ikkala_diagonal_yigindi(matrix))
+print("Uchinchi yuza yig`indisi : ",ikkita_uchburchak_yigindi(matrix))
+print("To'rtinchi yuza yig`indisi : ", aniq_romb_yigindi(matrix))
