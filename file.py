747d096fd6033c9ffb1e3d2ed423a2bf8e84d718
shubcape <shubcape@21-school.ru>
2025-12-16 13:01:45 +0500

    Ushbu dasturga matritsaning chetki elementlar yigindisini va diagonal elementlar yigindisini hisoblash funksiyalari qoshildi

---
+++ file.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+matrix = np.random.randint(1, 101, size=(9, 9))
+print(matrix)
+
+def chetki_yigindi(matrix: np.ndarray) -> int:
+    n, m = matrix.shape
+    
+    yigindi = matrix[0, :].sum() + matrix[n-1, :].sum()
+    
+    yigindi += matrix[1:n-1, 0].sum()
+    yigindi += matrix[1:n-1, m-1].sum()
+    
+    return yigindi
+
+def ikkala_diagonal_yigindi(matrix: np.ndarray) -> tuple[int, int]:
+    """
+    Asosiy va qarama-qarshi diagonal yig'indilarini qaytaradi.
+    """
+    asosiy = np.trace(matrix)
+    qarama_qarshi = np.trace(np.fliplr(matrix))
+    return asosiy + qarama_qarshi - matrix[4][4]
+
+print("Eng chetki hadlarining yig`indisi : ",chetki_yigindi(matrix))
+print("Giagonallarda joylashgan elementlar yig`indis : ",ikkala_diagonal_yigindi(matrix))
